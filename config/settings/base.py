08b911f4d372d8c252fa3b8df5b4b963d87177f2
askvart <tur1amant@gmail.com>
2022-07-22 17:59:45 +0300

    sss

--- config/settings/base.py
+++ config/settings/base.py
@@ -30,8 +30,8 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
-#SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = 'django-insecure-#lgguck8!4&tn43u=r+whso^(zznaeqq93$zgnrr_(4estsbq@'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 
 
